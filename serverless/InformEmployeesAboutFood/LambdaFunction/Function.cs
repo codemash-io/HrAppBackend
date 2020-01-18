1aea0a44b953997a0914b1717276faaa9fff823b
Domantas <domantasjovaisas@gmail.com>
2020-01-18 22:29:24 +0200

    Inform employees who ordered the food that food is arrived.

---
+++ serverless/InformEmployeesAboutFood/LambdaFunction/Function.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+using HrApp;
+using LambdaFunction.Inputs;
+using LambdaFunction.Services;
+using LambdaFunction.Settings;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace LambdaFunction
+{
+    public class Function
+    {
+        private readonly IExampleService _exampleService;
+        
+        // (Required if adding other constructors. Otherwise, optional.) A default constructor
+        // called by Lambda. If you are adding your custom constructors,
+        // default constructor with no parameters must be added
+        public Function() : this (new ExampleService()) {}
+
+        // (Optional) An example of injecting a service. As a default constructor is called by Lambda
+        // this constructor has to be called from default constructor
+        public Function(IExampleService exampleService)
+        {
+            _exampleService = exampleService;
+        }
+        
+        /// <summary>
+        /// (Required) Entry method of your Lambda function.
+        /// </summary>
+        /// <param name="lambdaEvent">Type returned from CodeMash</param>
+        /// <param name="context">Context data of a function (function config)</param>
+        /// <returns></returns>
+        public async Task<APIGatewayProxyResponse> Handler(CustomEventRequest<BasicInput> lambdaEvent, ILambdaContext context)
+        {
+            var repo = new MenuRepository();
+            var menu = await repo.GetClosestMenu();
+
+            var employees = await repo.GetEmployeesWhoOrderedFood(menu);
+            
+            var notificationService = new NotificationSender();
+            await notificationService.SendNotificationAboutFoodIsArrived(employees);
+        
+            
+            return new APIGatewayProxyResponse
+            {
+                Body = JsonConvert.SerializeObject(employees),
+                StatusCode = 200,
+                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+            };
+        }
+    }
+}
