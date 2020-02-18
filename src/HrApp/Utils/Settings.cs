768c2a9c58822d9111dad3bb9def6da132ad530c
Benas <benas.janusonis@outlook.com>
2020-02-18 12:22:11 +0200

    Merge conflict fix #3
    attempt at fixing merge conflict

--- src/HrApp/Utils/Settings.cs
+++ src/HrApp/Utils/Settings.cs
@@ -8,8 +8,8 @@ namespace HrApp
     {
         public static Guid ProjectId { get; set; } = Guid.Parse("b09eaa56-75eb-42f6-9d77-145ac6f6dedb");
         public static string ApiKey { get; set; } = Environment.GetEnvironmentVariable("ApiKey");
-        public static CodeMashClient Client { get; set; } = new CodeMashClient("96WLxsvp7FNolruRNIMYycgVT7rI4_Et", ProjectId);
         public static string ReminderAboutFoodTemplateId { get; set; } = "5e79f236-a85e-4833-8f98-581e626895bf";
         public static string FoodArrivedTemplateId { get; set; } = "35858c5a-45ce-4a86-826a-4aca77c35d8f";
+        public static CodeMashClient Client { get; set; } = new CodeMashClient(ApiKey, ProjectId);
     }
 }
