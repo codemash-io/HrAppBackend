092f04aef532a846c06ff6bd7afa727904c655a0
Benas <benas.janusonis@outlook.com>
2020-02-18 12:36:36 +0200

    Merge conflict fix #4
    fourth attempt at fixing merge conflict

--- src/HrApp/Contracts/IEmployeesRepository.cs
+++ src/HrApp/Contracts/IEmployeesRepository.cs
@@ -1,4 +1,5 @@
 using HrApp.Domain;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -7,9 +8,12 @@ namespace HrApp
     public interface IEmployeesRepository
     {
         Task<List<EmployeeEntity>> GetEmployees(Division division);
-
+        Task<List<EmployeeEntity>> GetEmployeesWhoAreNotInBussinessTrip(Division division, DateTime lunchDate);
         Task UpdateEmployeeTimeWorked(string employeeId, double time);
         Task<EmployeeEntity> GetEmployeeByLastName(string lastname);
         Task<EmployeeEntity> GetEmployeeById(string employeeId);
+
+        Task<List<EmployeeEntity>> GetEmployeesWhoNotAbsence(List<EmployeeEntity> employees, DateTime lunchDate);
+
     }
 }
