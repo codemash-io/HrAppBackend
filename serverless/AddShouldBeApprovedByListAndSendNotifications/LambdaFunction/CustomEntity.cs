8925400f6dbcbac83df099423f6db391707b22ac
CodeMash <cloud@codemash.io>
2020-03-05 08:58:39 +0200

    added new a function that adds required users with required roles to should be approved by list and sends notifications to them on new absence request creation in serverless folder (#13)
    Co-authored-by: Erikas <57006219+Metriksonas2@users.noreply.github.com>

---
+++ serverless/AddShouldBeApprovedByListAndSendNotifications/LambdaFunction/CustomEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodeMash.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+
+namespace HrApp
+{
+    public class CustomEntity : IEntity
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        [JsonProperty("_id")]
+        [Field("_id")]
+        public string Id { get; set; }
+    }
+}
