f8ad051cb8a0d78cc90400f17093ed90f0373881
M4nthas <mantas.daunoravicius@ktu.edu>
2020-04-07 11:52:37 +0300

    office365 calendar events lambdas added

---
+++ serverless/GetAllCalendarEvents/LambdaFunction/DTO/ProcessDTO.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace LambdaFunction
+{
+    public class ProcessDTO
+    {
+        [JsonProperty("meetingRoom")]
+        public string MeetingRoom { get; set; }
+        [JsonProperty("dateFrom")]
+        public DateTime DateFrom { get; set; }
+        [JsonProperty("dateTo")]
+        public DateTime DateTo { get; set; }
+    }
+}
