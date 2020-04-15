cf9dc62c5b5afe8a5fbffd980be037f01a99736f
Alauskas <37072295+Alauskas@users.noreply.github.com>
2020-04-15 17:08:17 +0300

    some fixes

--- src/HrApp/Entities/EmployeeNameSurnameEntity.cs
+++ src/HrApp/Entities/EmployeeNameSurnameEntity.cs
@@ -10,30 +10,13 @@ namespace HrApp
     //[Collection("pc-employees-testing")]
     public class EmployeeNameSurnameEntity : Entity
     {
-
         [Field("first_name")]
         public string FirstName { get; set; } = "";
 
         [Field("last_name")]
         public string LastName { get; set; } = "";
 
-        [Field("division")]
-        public string Division { get; set; } = "";
-
-        [Field("address")]
-        public string Address { get; set; } = "";
-        [Field("application_user")]
-        public Guid UserId { get; set; } = new Guid();
-
-        [Field("personal_identification_number")]
-        public string PersonalId { get; set; } = "";
-
-        [Field("manager")]
-        public string ManagerId { get; set; } = "";
-        [Field("business_email")]
-        public string BusinessEmail { get; set; } = "";
-        [Field("number_of_holidays_left")]
-        public int NumberOfHolidays { get; set; } = 0;  
+        
        
     }
 }
